4cf86ecf07554838dcb468b9723b03ad0a5d35db
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-08 18:58:15 +0300

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -6,14 +6,5 @@ Created on Tue Sep  5 23:23:52 2023
 @author: safaa
 """
 import streamlit as st
-#import joblib
-import pandas as pd
-import numpy as np
-st.title('Map of Safaa')
-# Defining Latitude and Longitude
-locate_map = pd.DataFrame(
-np.random.randn(50, 2)/[10,10] + [15.4589, 75.0078],
-columns = ['latitude', 'longitude'])
-#locate_map = pd.DataFrame([30.033333, 31.233334],columns = ['latitude', 'longitude'])
-# Map Function
-st.map(locate_map)
+number = st.slider("Pick a number: ", min_value=1, max_value=10)
+st.text("Your number is " + str(number))
