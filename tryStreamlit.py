a027ad336cc3fe856fdc0df928e59fad66c08994
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-09 14:30:18 +0300

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -6,5 +6,6 @@ Created on Tue Sep  5 23:23:52 2023
 @author: safaa
 """
 import streamlit as st
+import matplotib
 number = st.slider("Pick a number: ", min_value=1, max_value=10)
 st.text("Your number is " + str(number))
