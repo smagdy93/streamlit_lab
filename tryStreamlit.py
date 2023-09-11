a9e6d027b56a4f07856519fb3653b8d633d0b4d6
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-11 22:21:37 +0300

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -13,4 +13,13 @@ plt.show()
 
 number = st.slider("Pick a number: ", min_value=1, max_value=10)
 st.text("Your number is " + str(number))
+n1=st.number_input('First Width', min_value=1,value=1)
+n2=st.number_input('Second Width',min_value=1,value=1)
 st.balloons()
+col1, col2, col3 = st.beta_columns((n1,n2,n3))
+with col1:
+  st.write('First column')
+with col2:
+  st.write('Second column')
+with col3:
+  st.write('Third column')
