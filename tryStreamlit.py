35f2e8d0ed47cbe52941cab52c38e0c2ede6403e
Safaa Magdy <48313184+smagdy93@users.noreply.github.com>
2023-09-11 23:27:37 +0300

    Update tryStreamlit.py

--- tryStreamlit.py
+++ tryStreamlit.py
@@ -6,6 +6,7 @@ Created on Tue Sep  5 23:23:52 2023
 @author: safaa
 """
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
